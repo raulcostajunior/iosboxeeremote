73968c875c26d96573b485d9fcb9027e849386ee
Raul <raulcostajunior@mac-mini-de-raul.local>
2016-01-17 11:21:25 -0200

    Initial Commit

---
+++ BoxeeRemote/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BoxeeRemote
+//
+//  Created by Raul Costa Junior on 1/17/16.
+//  Copyright © 2016 Digital Streams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BoxeeRemote/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BoxeeRemote
+//
+//  Created by Raul Costa Junior on 1/17/16.
+//  Copyright © 2016 Digital Streams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
